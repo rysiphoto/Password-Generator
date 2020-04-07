b4155a38b0dcc27894b9971f53704c88982d311c
Ryan Siverson <rcsskier@mac.com>
2020-04-06 19:23:02 -0600

    moved to a viewable directory

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
+// var generateBtn = document.querySelector("#generate");
 var passwordValue = []; //Array to hold true or false returns from user input
 var passLength = prompt("Enter a number from 8 to 128 for the length of your password:");
 if (passLength < 8) {
   alert("Please enter a number between 8 and 128");
-} else if (passLength > 128) {
+} if (passLength > 128) {
   alert("Please enter a number between 8 and 128");
 } else {
   alert("Please enter a number.");
@@ -18,30 +18,30 @@ var passNum = confirm("Include Numbers?");
 console.log(passNum);
 var passSC = confirm("Include special characters?");
 console.log(passSC);
-// Pass a true or false value to passvalue array
+// Pass a true or false value to pass value array
 if (passLow === true) {
   passwordValue.push(passLow);
   console.log("lowercase letters");
 } else {
-  passwordValue[0] === false;
+  !passwordValue[0];
 }
 if (passUp === true) {
   passwordValue.push(passUp);
   console.log("uppercase letters");
 } else {
-  passwordValue[1] === false;
+  !passwordValue[1];
 }
 if (passNum === true) {
   passwordValue.push(passNum);
   console.log("numbers");
 } else {
-  passwordValue[2] === false;
+  !passwordValue[2];
 }
 if (passSC === true) {
   passwordValue.push(passSC);
   console.log("special characters");
 } else {
-  passwordValue[3] === false;
+  !passwordValue[3];
 }
 console.log(`Your ${passLength} character password includes: ${passwordValue}`)
 
@@ -67,15 +67,27 @@ if (passwordValue[3] === true) {
 
 
 console.log(passArr); //to log all characters going into random password
-// // function to jumble selected array options
-function generatePassword() {
-  var password = "";
-  for (var i = 0; i < passLength; i++)
-    password += passArr.length;
-  console.log(password);
-  return password;
+
+for (var i = 0; i < (passArr.length) * parseInt.passLength; i++) {
+
+  console.log(passArr[i]);
+  // var password = passArr[math.floor(math.random() * passArr.length)];
+  // console.log(password);
 }
+// function writePassword() {
+//   for (var i = 0; i < passLength; i++)
+//     var password = passArr[Math.floor(Math.random(passLength) * passArr.length)];
+//   console.log(password);
+// for (var i = 0; i < passArr.length; i++) {
+// console.log(passArr[i]);
+// }
+// var writePassword = passArr[Math.floor((Math.random(passLength) * 10) * passArr.length)];
+// console.log(writePassword);
+// // function to jumble selected array options
+// generateBtn.addEventListener("click", writePassword); {
+
+//   return password;
+// }
 
 
-// var generateBtn = document.querySelector("#generate");
 
